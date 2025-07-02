ab2654285a952069b858fe02064b58e80f739750
emanuel <emanuelbanol4@gmail.com>
2025-07-02 15:00:18 -0500

    actualizado

--- actividad 1.py
+++ actividad 1.py
@@ -1,80 +1,80 @@
-# """taller de introduccion a python"""
-# "________________________________1___________________________________"
-# nombre = input("introduce un nombre: ")
-# print (nombre)
-# "________________________________2___________________________________"
-# print (f"hola soy {nombre}")
-# "________________________________3___________________________________"
-# cuidad = input ("ingresa una cuidad: ")
-# print (cuidad)
-# "________________________________4___________________________________"
-# print (f"hola soy {nombre} y vivo en {cuidad}")
-# "________________________________5___________________________________"
-# color = input ("ingresa tu color favorito: ")
-# "________________________________6___________________________________"
-# print (f"tu color favorito es: {color}")
-# "________________________________7___________________________________"
-# animal = input("ingresa un animal: ")
-# print (f"animal")
-# "________________________________8___________________________________"
-# presentacion = (f"{nombre},{cuidad}")
-# print (presentacion)
-# "________________________________9____________________________________"
-# datos =  input("ingresa nombre: ")
-# datos += str(input("ingresa edad: "))
-# datos += input("ingresa cuidad: ")
-# datos += str(input("ingresa celular: "))
-# print(f"tus datos son: {datos}")
-# "________________________________10___________________________________"
-# num1=int(input("numero 1: "))
-# num2=int(input("numero 2: "))
-# num3=int(input("numero 3: "))
-# num4=int(input("numero 4: "))
-# num5=int(input("numero 5: "))
-# operacion = num1 * num2 / num3**num4
-# print (f"{operacion}")
-# "________________________________11____________________________________"
-# a= int(input("numero 1: "))
-# b= int(input("numero 2: "))
-# operacion = a+b
-# print (f"{operacion}")
-# "_________________________________12___________________________________"
-# operacion_2 = (a+b)
-# print (f"{operacion_2}")
-# "_________________________________13___________________________________"
-# operacion_3 = (a*b)
-# print (f"{operacion_3}")
-# "_________________________________14_____________________________________"
-# operacion_4 = (a/b)
-# print (f"{operacion_4}")
-# "__________________________________15__________________________________"
-# operacion_5 = (a**b)
-# print (f"{operacion_5}")
-# "__________________________________16____________________________________"
-# edad = int(input("ingresa edad: "))
-# print (f"{edad}")
-# "___________________________________17_________________________________"
-# operacion = edad + 10
-# print (f"{operacion}")
-# "_____________________________________18__________________________________"
-# num1 = int(input("ingresa 1° numero: "))
-# num2= int(input("ingresa 2° numero: "))
-# opera = num1 + num2
-# print (f"{opera}")
-# "___________________________________19________________________________"
-# opera2 = a % b
-# print (f"{opera2}")
+"""taller de introduccion a python"""
+"________________________________1___________________________________"
+nombre = input("introduce un nombre: ")
+print (f"tu nombre es: {nombre}")
+"________________________________2___________________________________"
+print (f"hola soy {nombre}")
+"________________________________3___________________________________"
+cuidad = input ("ingresa una cuidad: ")
+print (cuidad)
+"________________________________4___________________________________"
+print (f"hola soy {nombre} y vivo en {cuidad}")
+"________________________________5___________________________________"
+color = input ("ingresa tu color favorito: ")
+"________________________________6___________________________________"
+print (f"tu color favorito es: {color}")
+"________________________________7___________________________________"
+animal = input("ingresa un animal: ")
+print (f"animal")
+"________________________________8___________________________________"
+presentacion = (f"{nombre} , {cuidad}")
+print (f"hola, {presentacion}")
+"________________________________9____________________________________"
+datos =  input("ingresa nombre: ")
+datos += str(input("ingresa edad: "))
+datos += input("ingresa cuidad: ")
+datos += str(input("ingresa celular: "))
+print(f"tus datos son: {datos}")
+"________________________________10___________________________________"
+num1=int(input("numero 1: "))
+num2=int(input("numero 2: "))
+num3=int(input("numero 3: "))
+num4=int(input("numero 4: "))
+num5=int(input("numero 5: "))
+operacion = num1 * num2 / num3**num4
+print (f"la operacion es: {operacion}")
+"________________________________11____________________________________"
+a= int(input("numero 1: "))
+b= int(input("numero 2: "))
+operacion = a+b
+print (f"operacion 1: {operacion}")
+"_________________________________12___________________________________"
+operacion_2 = (a+b)
+print (f"operacion 2: {operacion_2}")
+"_________________________________13___________________________________"
+operacion_3 = (a*b)
+print (f"operacion 3: {operacion_3}")
+"_________________________________14_____________________________________"
+operacion_4 = (a/b)
+print (f"operacion 4: {operacion_4}")
+"__________________________________15__________________________________"
+operacion_5 = (a**b)
+print (f"operacion 5: {operacion_5}")
+"__________________________________16____________________________________"
+edad = int(input("ingresa edad: "))
+print (f"tu edad es: {edad}")
+"___________________________________17_________________________________"
+operacion = edad + 10
+print (f"tu edad dentro de 10 años: {operacion}")
+"_____________________________________18__________________________________"
+num1 = int(input("ingresa 1° numero: "))
+num2= int(input("ingresa 2° numero: "))
+opera = num1 + num2
+print (f"su suma es: {opera}")
+"___________________________________19________________________________"
+opera2 = a % b
+print (f"el moulo de {a} y {b} es: {opera2}")
 "___________________________________20____________________________"
-n1=int(input("1° numero: "))
-n2=int(input("2° numero: "))
-n3=int(input("3° numero: "))
+n1=int(input("1 numero: "))
+n2=int(input("2 numero: "))
+n3=int(input("3 numero: "))
 suma = n1 + n2 + n3
 promedio = suma / 3 
-print (f"{promedio}")
+print (f"su promedio es: {promedio}")
 "__________________________________21_____________________________"
 nombre = input ("dame tu nombre completo: ")
 nombre_completo= nombre
-print (f"{nombre_completo}")
+print (f"tu nombre completo es: {nombre_completo}")
 
 
 
