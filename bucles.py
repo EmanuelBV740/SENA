42cd7934b3252ab725ef606dbee3ef0937c5bfdb
emanuel bañol <emanuelbanol4@gmail.com>
2025-08-22 17:15:58 -0500

    nuevo

---
+++ bucles.py
@@ -0,0 +1,74 @@
+# #bucles 1º clase 
+# #while
+# num=int(input("dame un numero"))
+# while num > 1:
+#     print (f"{num}")
+#     num -= 1
+
+#print (f"finalizao el sistem")
+
+
+
+# #while - else
+# nmu_1 = int(input("ingrasa la tabla de multiplicar deseada: "))
+# i=1#iniciacion
+# print (f"\ninicia el contador en 1 {nmu_1}: ")
+# while i<= 100:
+#     print (f"{nmu_1}*{i}={nmu_1*i}")
+#     i+=1#actualizacion del numero 
+
+#break
+# i=int(input("dAME UN NUMERO: "))
+# while i<6:
+#     print(i)
+#     if i == 1:
+#         break
+# print (f"fin")
+
+x = 0
+while True:
+    x -= 1
+    print(x)
+    if x == 0:
+        break
+    
+print ("fin")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
